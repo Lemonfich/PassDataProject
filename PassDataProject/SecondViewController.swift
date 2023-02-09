a70cc58d3c1c3ee8f417e38132daaaa7dfbd3cb1
Sergei Mahov <Lemonfich@gmail.com>
2023-02-09 23:07:52 +0700

    finish

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var login: String?
+    
+    @IBOutlet weak var lableOutlet: UILabel!
+    
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "goHome", sender: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let login = self.login else { return }
+        lableOutlet.text = "Hello,\(login)"
+        
+        
     }
-    */
+    
 
 }
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -9,9 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NameLable: UILabel!
+    
+    @IBOutlet weak var loginFielderLable: UITextField!
+    
+    @IBOutlet weak var passwordFielderLable: UITextField!
+    
+    @IBOutlet weak var bottonOutlet: UIButton!
+    
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailsegue", sender: nil)
+        
+    }
+    
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        guard segue.identifier == "goHome" else { return }
+        guard let svc = segue.source as? SecondViewController else { return }
+        resultLable.text = svc.lableOutlet.text
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+       guard let dvc = segue.destination as? SecondViewController else { return }
+        dvc.login = loginFielderLable.text
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true )
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+
     }
 
 
